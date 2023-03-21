b0d40bc50c1ed651ae62a4858cf32baae390cf25
iamogbumitchel <mitchelogbu47@gmail.com>
2023-03-21 15:25:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const email = document.getElementById('email');
+const password2 = document.getElementById('password2')
+
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+
+  checkInputs();
+})
+
